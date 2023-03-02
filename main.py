1921933b2d5d9be43ee4f606af325ca4f8765dd0
Jejz168 <53441247+Jejz168@users.noreply.github.com>
2023-03-02 15:36:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ new Env('吾爱破解签到');
 """
 import os
 import sys
+import notify
 import urllib.parse
 import requests
 from bs4 import BeautifulSoup
@@ -63,11 +64,15 @@ for cookie in cookies.split("&"):
     jx_data = r_data.find("div", id="messagetext").find("p").text
     if "您需要先登录才能继续本操作" in jx_data:
         print(f"第{n}个账号Cookie 失效")
-        result += f"第{n}个账号Cookie 失效"
+        message = f"第{n}个账号Cookie 失效"
     elif "恭喜" in jx_data:
         print(f"第{n}个账号签到成功")
+        message = f"第{n}个账号签到成功"
     elif "不是进行中的任务" in jx_data:
         print(f"第{n}个账号今日已签到")
+        message = f"第{n}个账号今日已签到"
     else:
         print(f"第{n}个账号签到失败")
-
+        message = f"第{n}个账号签到失败"
+    n += 1
+    notify.send("吾爱签到", message)
