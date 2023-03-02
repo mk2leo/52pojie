2a68167cb36951026a552e715f02eae73c4ab46f
Jejz168 <53441247+Jejz168@users.noreply.github.com>
2023-03-02 13:43:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,15 +7,21 @@ from bs4 import BeautifulSoup
 def sign(cookie):
     result = ""
     headers = {
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,"
+                  "application/signed-exchange;v=b3;q=0.9",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "zh-CN,zh;q=0.9",
+        "Cache-Control": "no-cache",
+        "Connection": "keep-alive",
         "Cookie": cookie,
-        "ContentType": "text/html;charset=gbk",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                      "Chrome/109.0.0.0 Safari/537.36",
     }
     requests.session().put(
         "https://www.52pojie.cn/home.php?mod=task&do=draw&id=2", headers=headers
     )
     fa = requests.session().put(
-        "https://www.52pojie.cn/home.php?mod=task&do=apply&id=2", headers=headers
+        "https://www.52pojie.cn/home.php?mod=task&do=apply&id=2&referer=%2F", headers=headers
     )
     fb = BeautifulSoup(fa.text, "html.parser")
     fc = fb.find("div", id="messagetext").find("p").text
