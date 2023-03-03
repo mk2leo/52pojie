8c07a2569b316d4c0692a1931564a55b6e5abd0a
Jejz168 <53441247+Jejz168@users.noreply.github.com>
2023-03-03 13:45:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,6 @@ from bs4 import BeautifulSoup
 
 # 多cookie使用&分割
 cookies = ""
-pushapi = os.environ.get("PUSH_PLUS_TOKEN")
 if cookies == "":
     if os.environ.get("POJIE"):
         cookies = os.environ.get("POJIE")
@@ -65,18 +64,15 @@ for cookie in cookies.split("&"):
     jx_data = r_data.find("div", id="messagetext").find("p").text
     if "您需要先登录才能继续本操作" in jx_data:
         print(f"第{n}个账号Cookie 失效")
-        msg = f"第{n}个账号Cookie 失效"
+        message = f"第{n}个账号 Cookie 失效"
     elif "恭喜" in jx_data:
         print(f"第{n}个账号签到成功")
-        msg = f"第{n}个账号签到成功"
+        message = f"第{n}个账号 签到成功"
     elif "不是进行中的任务" in jx_data:
         print(f"第{n}个账号今日已签到")
-        msg = f"第{n}个账号今日已签到"
+        message = f"第{n}个账号 今日已签到"
     else:
         print(f"第{n}个账号签到失败")
-        msg = f"第{n}个账号签到失败"
-        requests.get('http://www.pushplus.plus/send?token=' + pushapi + '&title='+msg+'&content='+msg)
-
-if __name__ == "__main__":
+        message = f"第{n}个账号 签到失败"
     n += 1
-    notify.send("吾爱签到", msg)
+    notify.send("吾爱签到", message)
